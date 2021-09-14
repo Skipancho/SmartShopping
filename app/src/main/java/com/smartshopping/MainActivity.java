abf742447524628f2d8f54b5b6be5b51a9f02c73
Skipancho <ctg1770a@gmail.com>
2021-09-14 18:49:20 +0900

    pause -> stop

--- app/src/main/java/com/smartshopping/MainActivity.java
+++ app/src/main/java/com/smartshopping/MainActivity.java
@@ -382,12 +382,12 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public void run() {
-            int i = 0;
+            //int i = 0;
             while (running){
                 Token_Check(myToken);
                 try{
                     this.sleep(3000);
-                    System.out.println("running"+(i++));
+                    //System.out.println("running"+(i++));
                 }catch (Exception e){
                     e.printStackTrace();
                 }
